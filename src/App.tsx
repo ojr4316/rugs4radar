c5f5b908487385494b6e1a5d611f0320f31278de
Owen <ojr4316@rit.edu>
2023-09-20 15:00:47 -0400

    added placeholder project description and tags

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,68 @@ import { news } from "./fillOut";
 import Team from "./Team";
 import Week from "./Week";
 
-
 function getPageContents(page: string) {
   switch (page) {
-    case "news": 
+    case "home":
+      return (
+        <div style={{ width: "40%", marginTop: "32px" }}>
+          <p className="page-title">Project Description</p>
+          <p className="page-text">
+            1) The TDA4VM board from Texas Instruments is confirmed to work with
+            ROS Our project with D3 engineering has a MVP the scope of it is as
+            follows: 2. Our first task is to follow the documentation to
+            replicate this set up. The most general overview of this system is
+            the TI board running ROS 2 and an external PC also running ROS 2.
+            These 2 communicate through SSH.
+          </p>
+          <p className="page-text">
+            2) The functionality of system using the TI board needs to be
+            documented so that the behavior can be replicated latter on.
+          </p>
+          <p className="page-text">
+            3) D3's product uses the same micro chip as the TI board but the pcb
+            is different. The functionally of the system when using the TI board
+            needs to be replicated on the system when using the D3 board. To do
+            this a branch of ROS 2 will be made.
+          </p>
+          <p className="page-text">
+            There are more goals in the project but replicating the behavior of
+            the TI board for the D3 board is the MVP of the project and they
+            will be addressed at a later date if at all.
+          </p>
+
+          <p className="page-title" style={{ marginTop: "48px" }}>
+            Project Tags
+          </p>
+          <p className="page-text">
+            Robotics, Vision, AI, Docker, Embedded Systems
+          </p>
+        </div>
+      );
+    case "news":
       return news;
     case "week":
-      return <Week/>;
+      return <Week />;
     case "team":
-      return <Team/>;
+      return <Team />;
     default:
-      return <p> Error! </p>
+      return <p> Error! </p>;
   }
 }
 
 export default function App() {
-  const [page, setPage] = useState("news");
+  const [page, setPage] = useState("home");
 
   return (
     <div className="page-container">
-      <div className="senior-project-header">
+      <div className="senior-project-header" onClick={() => setPage("home")}>
         <p> RIT </p>
         <p> Rugs4Radar </p>
         <p> SE Senior Project </p>
       </div>
 
       <div className="nav">
-        <div>
+        <div onClick={() => setPage("home")}>
           <h4 className="page-subtitle">D3 Engineering -</h4>
           <h3 className="page-title"> Rugged Vision and Radar Platform</h3>
         </div>
@@ -56,9 +91,7 @@ export default function App() {
         </div>
       </div>
 
-      <div className="page-content">
-        {getPageContents(page)}
-      </div>
+      <div className="page-content">{getPageContents(page)}</div>
     </div>
   );
 }
