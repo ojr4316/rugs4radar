113881b6252c77934d9c73779cf6a46ab4f5f164
William Dabney <80066372+whd8254@users.noreply.github.com>
2023-10-24 10:13:41 -0400

    added more structure to sudo code

--- src/scripts/python/build.py
+++ src/scripts/python/build.py
@@ -1,17 +1,40 @@
 import argparse
+class TIboard:
+    dir = ""
 
-def build(dir, partion ):
+    def __init__(self, mode, partition):
+        """"""
+        
+        if 's' in mode:
+            """"""
+            self.setup()
+        if 'b' in mode:
+            """"""
+            self.build()
+        if 'f' in mode:
+            """"""
+            self.flash(partition)
 
+    def setup(self):
+        """setup.sh"""
+    def build(self):
+        """create root and boot directories"""
+        """make u-boot"""
+        """make linux"""
+        """make u-boot_install"""
+        """make linux_install"""
+    def flash(self, partition):
+        """"""
 
 def parse_args():
     parser=argparse.ArgumentParser(description="setting up environment and or building image")
 
     
-    parser.add_argument('-dir', '--directory', type=str, help="to the base folder in which all files live")
-    parser.add_argument('-bf', '--bin_file', type=str, required=False)
+    # parser.add_argument('-bf', '--bin_file', type=str, required=False)
 def main():
-    inputs=parse_args()
-    print(inputs)
+    # inputs=parse_args()
+    # print(inputs)
+    TIboard()
 
 if __name__ == '__main__':
     main()
